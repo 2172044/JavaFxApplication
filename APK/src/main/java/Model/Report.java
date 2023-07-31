2e4098777ff4f62c132a00b70e5318066afe3191
Theresia <90782555+2172017@users.noreply.github.com>
2023-07-31 10:28:43 +0800

    Add files via upload

---
+++ APK/src/main/java/Model/Report.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Report {
+    private Integer Tglpembelian;
+    private String NoFaktur;
+    private String NoPelanggan ;
+    private Float Harga ;
+    private String JenisRumah;
+    private String
+}
