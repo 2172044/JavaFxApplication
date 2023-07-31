988edb426f910854b143c89cf35963b36e635781
Kristiani Nainggolan <90815204+2172044@users.noreply.github.com>
2023-07-31 08:25:02 +0700

    Add files via upload

---
+++ TugasBesar/src/main/java/com/TugasBesar/LogInController.java
@@ -0,0 +1,22 @@
+package com.TugasBesar;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class LogInController {
+    //@FXML
+    //private Label welcomeText;
+
+    //@FXML
+    //rotected void onHelloButtonClick() {
+      //  welcomeText.setText("Welcome to JavaFX Application!");
+    //}
+
+}
